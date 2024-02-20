76ebbb444875e4d3dc4ed3d3ba54e2698cce2e9d
Jakub Pawlak <KubaPawlak1051@wp.pl>
2024-02-20 23:29:26 +0100

    feat: create reusable input component

--- src/components/Layout/Header/SearchInput.tsx
+++ src/components/Layout/Header/SearchInput.tsx
@@ -5,8 +5,8 @@ import { IoMdSearch } from "react-icons/io";
 
 import { useRouter, useSearchParams } from "next/navigation";
 
+import { Input } from "@/components/UI/Input";
 import { useDebounce } from "@/hooks/useDebounce";
-import { cn } from "@/lib/utils";
 
 type Props = {
 	id: string;
@@ -20,42 +20,29 @@ export const SearchInput = ({ id, className }: Props) => {
 	const debouncedValue = useDebounce(searchValue, 500);
 
 	const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-		if (value === "") {
-			router.back();
-		}
-
 		setSearchValue(value);
 	};
 
 	useEffect(() => {
-		if (debouncedValue) {
+		if (debouncedValue.trim().length > 2) {
 			router.push(`/search?query=${debouncedValue.trim()}`);
 		}
 	}, [debouncedValue, router]);
 
 	return (
-		<div className={cn("w-full max-w-lg lg:max-w-xs", className)}>
-			<label htmlFor={id} className="sr-only">
-				Search for products
-			</label>
-			<div className="relative">
-				<span className="absolute inset-y-0 left-0 ms-2 flex items-center">
-					<IoMdSearch className="h-5 w-5 text-gray-400" aria-hidden />
-				</span>
-				<input
-					className="w-full rounded-md border-0 bg-twilight-50 py-2 pl-11 pr-4 text-sm text-slate-800 ring-1 ring-inset ring-gray-300 placeholder:text-slate-400 focus:ring-2 focus:ring-inset focus:ring-skyfall-500"
-					type="search"
-					id={id}
-					name="search-for-products"
-					placeholder="Search for products..."
-					value={searchValue}
-					onChange={handleChange}
-					autoComplete="off"
-					autoCorrect="off"
-					autoCapitalize="off"
-					spellCheck="false"
-				/>
-			</div>
-		</div>
+		<Input
+			type="search"
+			id={id}
+			name="search-for-products"
+			value={searchValue}
+			onChange={handleChange}
+			onBlur={() => setSearchValue(searchParams.get("query") || "")}
+			placeholder="Search"
+			className={className}
+			Icon={IoMdSearch}
+			label="Search for Products"
+			autoCorrect="off"
+			spellCheck="false"
+		/>
 	);
 };

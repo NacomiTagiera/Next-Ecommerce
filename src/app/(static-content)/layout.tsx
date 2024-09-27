f72fd5d9715bb662cbdaada069fc9260f623535f
Jakub Pawlak <KubaPawlak1051@wp.pl>
2024-09-27 23:16:38 +0200

    refactor: add more content to legal pages, create Cookies page

--- src/app/(static-content)/layout.tsx
+++ src/app/(static-content)/layout.tsx
@@ -1,3 +1,7 @@
 export default function StaticLayout({ children }: { children: React.ReactNode }) {
-	return <div className="mx-auto max-w-md text-center">{children}</div>;
+	return (
+		<div className="container mx-auto px-4 py-10 sm:px-6 lg:px-8">
+			<div className="mx-auto w-full max-w-5xl p-6">{children}</div>
+		</div>
+	);
 }

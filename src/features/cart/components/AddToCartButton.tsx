946e62ddc7889392e04fd730aaed9768d479328e
Jakub Pawlak <KubaPawlak1051@wp.pl>
2024-10-12 23:57:54 +0200

    feat: display fixed AddToCartButton to bottom on mobiles
    Utilize new useIntersectionObserver to display the AddToCartButton fixed to the bottom of the screen on mobile devices when the user scrolls past the AddToCartButton.

--- src/features/cart/components/AddToCartButton.tsx
+++ src/features/cart/components/AddToCartButton.tsx
@@ -6,26 +6,46 @@ import { FiShoppingBag } from "react-icons/fi";
 import { useRouter } from "next/navigation";
 
 import { ActionButton } from "@/components/UI/ActionButton";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 
 import { addItemToCart } from "../api/actions";
 
 export const AddToCartButton = ({ itemId }: { itemId: string }) => {
+	const { visible, ref } = useIntersectionObserver<HTMLFormElement>({ threshold: 0.4 });
 	const [message, formAction] = useFormState(addItemToCart, null);
 	const router = useRouter();
 
 	return (
-		<form action={formAction}>
-			<input type="text" name="itemId" value={itemId} hidden readOnly />
-			<ActionButton
-				leadingIcon={FiShoppingBag}
-				onClick={() => router.push("/cart")}
-				className="mt-10 px-8 sm:max-w-xs"
-			>
-				Add to Cart
-			</ActionButton>
-			<p aria-live="polite" className="sr-only" role="status">
-				{message}
-			</p>
-		</form>
+		<>
+			<form action={formAction} ref={ref}>
+				<input type="text" name="itemId" value={itemId} hidden readOnly />
+				<ActionButton
+					leadingIcon={FiShoppingBag}
+					className="mt-10 px-8 sm:max-w-xs"
+					onClick={() => router.push("/cart")}
+				>
+					Add to Cart
+				</ActionButton>
+				<p aria-live="polite" className="sr-only" role="status">
+					{message}
+				</p>
+			</form>
+
+			{!visible && (
+				<form action={formAction} className="fixed bottom-0 left-0 right-0 z-10 sm:hidden">
+					<input type="text" name="itemId" value={itemId} hidden readOnly />
+					<ActionButton
+						leadingIcon={FiShoppingBag}
+						className="rounded-none"
+						onClick={() => router.push("/cart")}
+					>
+						Add to Cart
+					</ActionButton>
+					<p aria-live="polite" className="sr-only" role="status">
+						{message}
+					</p>
+				</form>
+			)}
+		</>
 	);
 };
